f319572f79e96e2e58f6a5911a5e669ef8540f61
RechnoyBobr <someshitcoding@list.ru>
2024-01-20 22:40:10 +0300

    Typst upgrade

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,18 @@
 return {
+<<<<<<< HEAD
   "smjonas/inc-rename.nvim",
   cmd = "IncRename",
   config = true,
+=======
+  { "smjonas/inc-rename.nvim", cmd = "IncRename", config = true },
+  { "kaarmu/typst.vim", ft = "typst", lazy = false },
+  {
+    "chomosuke/typst-preview.nvim",
+    lazy = false, -- or ft = 'typst'
+    version = "0.1.*",
+    build = function()
+      require("typst-preview").update()
+    end,
+  },
+>>>>>>> 8bac8dc (Typst upgrade)
 }
